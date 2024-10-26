479bcc635aef2730be07b042f79163db6b611a7a
POAPCA02LC0107 <SSDD@POAPCA02LC0107.duoc.acad>
2024-10-26 11:36:24 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'KarlOctave_Parcial02',
+  webDir: 'www'
+};
+
+export default config;
